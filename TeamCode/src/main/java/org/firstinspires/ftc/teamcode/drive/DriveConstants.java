e70b2396322c278f89cbfdb98197d4abf59a4aae
anirudhsnayak <anirudhsnayak@gmail.com>
2021-02-14 19:22:55 -0800

    Roadrunner test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -21,7 +21,7 @@ public class DriveConstants {
      * These are motor constants that should be listed online for your motors.
      */
     public static final double TICKS_PER_REV = 1220;
-    public static final double MAX_RPM = 20;
+    public static final double MAX_RPM = 120;
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
